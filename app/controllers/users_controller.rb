00e6fdce3556504a8624bb9841f69b66074a509e
Emily-Waters <emilyabigailwaters@gmail.com>
2022-02-03 17:50:35 +0000

    Add user controller. Add session controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # throw user_params
+
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
+  def encrypt_password
+    #TODO bcrypt password here
+  end
+
+end
