bcb8da0dc6b22a93a953d8f175f60aea585e2ae2
Emily-Waters <emilyabigailwaters@gmail.com>
2022-02-07 21:18:25 +0000

    Add downcase for email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
+    params[:user][:email].downcase!
     params.require(:user).permit(
       :first_name,
       :last_name,
